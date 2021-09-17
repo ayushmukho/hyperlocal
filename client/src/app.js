0c63ab9ff3be616a8ee92696ddd04df56ece8b01
ayushmukho <ayushmukho@gmail.com>
2021-09-18 00:16:18 +0530

    Minor Changes

--- client/src/app.js
+++ client/src/app.js
@@ -3,18 +3,25 @@ import { useSelector, useDispatch } from "react-redux";
 import { getProducts } from "./actions/products";
 const App = () => {
   const parcel = useSelector((state) => state);
-  const { products: { products, isLoading } } = parcel;
+  const {
+    products: { products, isLoading },
+  } = parcel;
   const dispatch = useDispatch();
 
   useEffect(() => {
-   dispatch(getProducts())
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    dispatch(getProducts());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
+  // console.log("Product: ", parcel)
   console.log("Products: ", products);
   console.log("Loading: ", isLoading);
 
-  return <div>Hello world!</div>;
+  return isLoading ? (
+    <h1>Kuch nahi h randi....Loading</h1>
+  ) : (
+    products.map((product, i) => <h1 key={i}>{product.title}</h1>)
+  );
 };
 
 export default App;
