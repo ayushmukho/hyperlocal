1da01c165422392ada5cc414840280a226498550
priyadarshss <priyadarsh2001@gmail.com>
2021-10-02 16:08:37 +0530

    deleted unwanted code

--- client/src/components/LandingPage/Cards/Cards.js
+++ client/src/components/LandingPage/Cards/Cards.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
-import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import Link from '@material-ui/core/Link'
 
@@ -52,15 +50,6 @@ export default function Cards() {
 
   const Redirect = () => (
     <>
-      {/* <CardContent>
-        <Typography gutterBottom variant='h5' component='h2'>
-          Redirect
-        </Typography>
-        <Typography variant='body2' color='textSecondary' component='p'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </Typography>
-      </CardContent> */}
       <Link href='/signup' className='btn btn-primary'>
         Sign up
       </Link>
