a9673452b177aa55a4a53a50e756a84cb9bbafa6
shuhaib sherif <shuhaibsherif@gmail.com>
2021-10-13 20:12:55 +0530

    signIn and signUp modal

--- client/src/components/LandingPage/Navbar/Navbar.js
+++ client/src/components/LandingPage/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FaBars } from "react-icons/fa";
 import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
@@ -7,39 +7,21 @@ import { useState } from "react";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 
-import {
-  Grid,
-  Typography,
-  Box,
-  Container,
-  TextField,
-  FormControl,
-  InputLabel,
-  Input,
-  InputAdornment,
-  IconButton,
-  Button,
-} from "@material-ui/core";
-import { toast } from "react-toastify";
+import { Button } from "@material-ui/core";
+
 import { useDispatch, useSelector } from "react-redux";
-import { GoogleLogin } from "react-google-login";
-import gb from "../../../images/google.png";
 import Avatar from "@material-ui/core/Avatar";
 
 import "react-toastify/dist/ReactToastify.css";
 
-import { useHistory } from "react-router-dom";
-
-import { Visibility, VisibilityOff } from "@material-ui/icons";
-
 import useStyles from "./styles";
 
-import sally from "../../../images/sally.png";
-import ellipse from "../../../images/ellipse.png";
-import * as api from "../../../api/index";
-import { googleLogin } from "../../../actions/auth";
 import Modal from "@material-ui/core/Modal";
 import { LOGOUT } from "../../../constants/actionTypes";
+
+import SignUp from "../../authentication/SignUp/SignUp";
+import Mod from "../../Helpers/Modal/modal";
+
 
 const Nav = styled.nav`
   background: none;
@@ -89,62 +71,27 @@ const NavMenu = styled.div`
 
 const Navbar = () => {
   const classes = useStyles();
-  const history = useHistory();
+
   const dispatch = useDispatch();
-  const [showPassword, setShowPassword] = useState(false);
-  const [values, setValues] = useState({
-    password: "",
-    email: "",
-    username: "",
-  });
 
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
-
-  const handleClickShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const { data } = await api.signup(values);
-      toast(data.message);
-    } catch (error) {
-      toast(error.response.data.message);
-    }
-  };
-
-  const googleSuccess = async (res) => {
-    const token = res.tokenId;
-    try {
-      dispatch(googleLogin(token, history));
-    } catch (error) {
-      console.log(error.response.data);
-    }
-  };
-
-  const googleError = () =>
-    toast("Google Sign In was unsuccessful. Try again later");
-
-  const [open, setOpen] = useState(false);
+  const [openSignUp, setOpenSignUp] = useState(false);
+ 
   const handleOpen = () => {
-    setOpen(true);
-    console.log("btnitnfsjdnvj");
+    setOpenSignUp(true);
+  
   };
 
   const handleClose = () => {
-    setOpen(false);
+    setOpenSignUp(false);
   };
+
+ 
+
   const sellersData = useSelector((state) => state.user);
   const { isLoading, authData } = sellersData;
 
   const [anchorEl, setAnchorEl] = useState(null);
+  
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -158,6 +105,7 @@ const Navbar = () => {
     dispatch({ type: LOGOUT });
     setAnchorEl(null);
   };
+
 
   return (
     <>
@@ -197,146 +145,13 @@ const Navbar = () => {
         ) : (
           <>
             <Button onClick={handleOpen} className={classes.but}>
-              Sign in
+              Sign Up
             </Button>
-            <Modal open={open} onClose={handleClose} className={classes.modal}>
-              <Container className={classes.toplevel1}>
-                <Grid container className={classes.toplevel2}>
-                  <Grid item md={5} className={classes.sublevel1}>
-                    <div className={classes.hide}>
-                      <img alt="logo" className={classes.logo} src={logo} />
-
-                      <Typography className={classes.text}>
-                        Find all types of everyday items at your arms reach{" "}
-                      </Typography>
-
-                      <img className={classes.sally} alt="sally" src={sally} />
-                      <img
-                        className={classes.ellipse}
-                        alt="ellipse"
-                        src={ellipse}
-                      />
-                    </div>
-                  </Grid>
-
-                  <Grid item xs={12} md={7} className={classes.sublevel2}>
-                    <Box className={classes.boxExpand}>
-                      <Typography
-                        component="h1"
-                        variant="h5"
-                        style={{ fontSize: "36px", fontWeight: 700 }}
-                      >
-                        Create Account
-                      </Typography>
-                      <GoogleLogin
-                        clientId="390268815880-iesar9tq1omf7a47b3lhnrp5hf6bah1l.apps.googleusercontent.com"
-                        onSuccess={googleSuccess}
-                        onFailure={googleError}
-                        cookiePolicy="single_host_origin"
-                        render={(renderProps) => (
-                          <Button
-                            className={classes.googleButton}
-                            onClick={renderProps.onClick}
-                            disabled={renderProps.disabled}
-                            variant="contained"
-                          >
-                            <div>
-                              <img
-                                src={gb}
-                                className={classes.googleImage}
-                                alt="google"
-                              />
-                            </div>{" "}
-                            <div>Google Sign In</div>
-                          </Button>
-                        )}
-                      />
-                      <Box
-                        component="form"
-                        noValidate
-                        onSubmit={handleSubmit}
-                        sx={{
-                          px: 5,
-                          mt: 3,
-                        }}
-                      >
-                        <TextField
-                          margin="normal"
-                          required
-                          fullWidth
-                          id="fullname"
-                          label="Full Name"
-                          variant="standard"
-                          onChange={handleChange("username")}
-                        />
-                        <TextField
-                          margin="normal"
-                          required
-                          fullWidth
-                          id="email"
-                          label="Email"
-                          variant="standard"
-                          onChange={handleChange("email")}
-                        />
-
-                        <FormControl
-                          required
-                          fullWidth
-                          margin="normal"
-                          variant="standard"
-                        >
-                          <InputLabel htmlFor="adornment-password">
-                            Password
-                          </InputLabel>
-                          <Input
-                            id="adornment-password"
-                            type={showPassword ? "text" : "password"}
-                            value={values.password}
-                            onChange={handleChange("password")}
-                            endAdornment={
-                              <InputAdornment position="end">
-                                <IconButton
-                                  aria-label="toggle password visibility"
-                                  onClick={handleClickShowPassword}
-                                  onMouseDown={handleMouseDownPassword}
-                                  edge="end"
-                                >
-                                  {showPassword ? (
-                                    <VisibilityOff />
-                                  ) : (
-                                    <Visibility />
-                                  )}
-                                </IconButton>
-                              </InputAdornment>
-                            }
-                          />
-                        </FormControl>
-
-                        <Button
-                          type="submit"
-                          className={classes.submitButton}
-                          fullWidth
-                          variant="contained"
-                          sx={{ mt: 3, mb: 2 }}
-                        >
-                          Create Account
-                        </Button>
-                        <Grid container style={{ marginTop: "10px" }}>
-                          <Grid item>
-                            <NavLink
-                              to="/login"
-                              style={{ textDecoration: "none" }}
-                            >
-                              {"Already have an account?"}
-                            </NavLink>
-                          </Grid>
-                        </Grid>
-                      </Box>
-                    </Box>
-                  </Grid>
-                </Grid>
-              </Container>
-            </Modal>
+           
+              <SignUp openSignUp={openSignUp} 
+              setOpenSignUp={setOpenSignUp}
+              Close={handleClose} />
+           
           </>
         )}
       </Nav>
--- client/src/components/authentication/SignUp/SignUp.js
+++ client/src/components/authentication/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import {
   Grid,
   Typography,
@@ -11,134 +11,139 @@ import {
   InputAdornment,
   IconButton,
   Button,
-} from '@material-ui/core'
-import { toast } from 'react-toastify'
-import { useDispatch } from 'react-redux'
-import { GoogleLogin } from 'react-google-login'
-import gb from '../../../images/google.png'
+  Modal,
+} from "@material-ui/core";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { GoogleLogin } from "react-google-login";
+import gb from "../../../images/google.png";
 
-import 'react-toastify/dist/ReactToastify.css'
+import "react-toastify/dist/ReactToastify.css";
 
-import { Link, useHistory } from 'react-router-dom'
+import { Link, useHistory } from "react-router-dom";
 
-import { Visibility, VisibilityOff } from '@material-ui/icons'
+import { Visibility, VisibilityOff } from "@material-ui/icons";
 
-import useStyles from '../../LandingPage/Navbar/styles'
+import useStyles from "../../LandingPage/Navbar/styles";
 
-import sally from '../../../images/sally.png'
-import logo from '../../../images/logo.png'
-import ellipse from '../../../images/ellipse.png'
-import * as api from '../../../api/index'
-import { googleLogin } from '../../../actions/auth'
-import Modal from '@material-ui/core/Modal'
+import sally from "../../../images/sally.png";
+import logo from "../../../images/logo.png";
+import ellipse from "../../../images/ellipse.png";
+import * as api from "../../../api/index";
+import { googleLogin } from "../../../actions/auth";
+import SignIn from "../SignIn/SignIn";
 
-const SignUp = () => {
-  const classes = useStyles()
-  const history = useHistory()
-  const dispatch = useDispatch()
-  const [showPassword, setShowPassword] = useState(false)
+var linkClicked = "0";
+const SignUp = ({ openSignUp, Close, setOpenSignUp }) => {
+  const classes = useStyles();
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const [showPassword, setShowPassword] = useState(false);
   const [values, setValues] = useState({
-    password: '',
-    email: '',
-    username: '',
-  })
+    password: "",
+    email: "",
+    username: "",
+  });
 
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value })
-  }
+  const [open, setOpen] = useState(false);
 
-  const handleClickShowPassword = () => {
-    setShowPassword(!showPassword)
-  }
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault()
-  }
-
-  const handleSubmit = async (event) => {
-    event.preventDefault()
-    try {
-      const { data } = await api.signup(values)
-      toast(data.message)
-    } catch (error) {
-      toast(error.response.data.message)
-    }
-  }
-
-  const googleSuccess = async (res) => {
-    const token = res.tokenId
-    try {
-      dispatch(googleLogin(token, history))
-    } catch (error) {
-      console.log(error.response.data)
-    }
-  }
-
-  const googleError = () =>
-    toast('Google Sign In was unsuccessful. Try again later')
-
-  const [open, setOpen] = useState(false)
   const handleOpen = () => {
-    setOpen(true)
-  }
+    setOpen(true);
+    setOpenSignUp(false);
+  };
 
   const handleClose = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+    setOpenSignUp(false)
+  };
 
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await api.signup(values);
+      toast(data.message);
+    } catch (error) {
+      toast(error.response.data.message);
+    }
+  };
+
+  const googleSuccess = async (res) => {
+    const token = res.tokenId;
+    try {
+      dispatch(googleLogin(token, history));
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
+  const googleError = () =>
+    toast("Google Sign In was unsuccessful. Try again later");
+
+  
   return (
     <>
-      <Button onClick={handleOpen}>Open Modal</Button>
-      <Modal open={open} onClose={handleClose} className={classes.modal}>
+      <Modal open={openSignUp} onClose={Close} className={classes.modal}>
         <Container className={classes.toplevel1}>
           <Grid container className={classes.toplevel2}>
             <Grid item md={5} className={classes.sublevel1}>
               <div className={classes.hide}>
-                <img alt='logo' className={classes.logo} src={logo} />
+                <img alt="logo" className={classes.logo} src={logo} />
 
                 <Typography className={classes.text}>
-                  Find all types of everyday items at your arms reach{' '}
+                  Find all types of everyday items at your arms reach{" "}
                 </Typography>
 
-                <img className={classes.sally} alt='sally' src={sally} />
-                <img className={classes.ellipse} alt='ellipse' src={ellipse} />
+                <img className={classes.sally} alt="sally" src={sally} />
+                <img className={classes.ellipse} alt="ellipse" src={ellipse} />
               </div>
             </Grid>
 
             <Grid item xs={12} md={7} className={classes.sublevel2}>
               <Box className={classes.boxExpand}>
                 <Typography
-                  component='h1'
-                  variant='h5'
-                  style={{ fontSize: '36px', fontWeight: 700 }}
+                  component="h1"
+                  variant="h5"
+                  style={{ fontSize: "36px", fontWeight: 700 }}
                 >
                   Create Account
                 </Typography>
                 <GoogleLogin
-                  clientId='390268815880-iesar9tq1omf7a47b3lhnrp5hf6bah1l.apps.googleusercontent.com'
+                  clientId="390268815880-iesar9tq1omf7a47b3lhnrp5hf6bah1l.apps.googleusercontent.com"
                   onSuccess={googleSuccess}
                   onFailure={googleError}
-                  cookiePolicy='single_host_origin'
+                  cookiePolicy="single_host_origin"
                   render={(renderProps) => (
                     <Button
                       className={classes.googleButton}
                       onClick={renderProps.onClick}
                       disabled={renderProps.disabled}
-                      variant='contained'
+                      variant="contained"
                     >
                       <div>
                         <img
                           src={gb}
                           className={classes.googleImage}
-                          alt='google'
+                          alt="google"
                         />
-                      </div>{' '}
+                      </div>{" "}
                       <div>Google Sign In</div>
                     </Button>
                   )}
                 />
                 <Box
-                  component='form'
+                  component="form"
                   noValidate
                   onSubmit={handleSubmit}
                   sx={{
@@ -147,45 +152,45 @@ const SignUp = () => {
                   }}
                 >
                   <TextField
-                    margin='normal'
+                    margin="normal"
                     required
                     fullWidth
-                    id='fullname'
-                    label='Full Name'
-                    variant='standard'
-                    onChange={handleChange('username')}
+                    id="fullname"
+                    label="Full Name"
+                    variant="standard"
+                    onChange={handleChange("username")}
                   />
                   <TextField
-                    margin='normal'
+                    margin="normal"
                     required
                     fullWidth
-                    id='email'
-                    label='Email'
-                    variant='standard'
-                    onChange={handleChange('email')}
+                    id="email"
+                    label="Email"
+                    variant="standard"
+                    onChange={handleChange("email")}
                   />
 
                   <FormControl
                     required
                     fullWidth
-                    margin='normal'
-                    variant='standard'
+                    margin="normal"
+                    variant="standard"
                   >
-                    <InputLabel htmlFor='adornment-password'>
+                    <InputLabel htmlFor="adornment-password">
                       Password
                     </InputLabel>
                     <Input
-                      id='adornment-password'
-                      type={showPassword ? 'text' : 'password'}
+                      id="adornment-password"
+                      type={showPassword ? "text" : "password"}
                       value={values.password}
-                      onChange={handleChange('password')}
+                      onChange={handleChange("password")}
                       endAdornment={
-                        <InputAdornment position='end'>
+                        <InputAdornment position="end">
                           <IconButton
-                            aria-label='toggle password visibility'
+                            aria-label="toggle password visibility"
                             onClick={handleClickShowPassword}
                             onMouseDown={handleMouseDownPassword}
-                            edge='end'
+                            edge="end"
                           >
                             {showPassword ? <VisibilityOff /> : <Visibility />}
                           </IconButton>
@@ -195,19 +200,30 @@ const SignUp = () => {
                   </FormControl>
 
                   <Button
-                    type='submit'
+                    type="submit"
                     className={classes.submitButton}
                     fullWidth
-                    variant='contained'
+                    variant="contained"
                     sx={{ mt: 3, mb: 2 }}
                   >
                     Create Account
                   </Button>
-                  <Grid container style={{ marginTop: '10px' }}>
+                  <Grid container style={{ marginTop: "10px" }}>
                     <Grid item>
-                      <Link to='/login' style={{ textDecoration: 'none' }}>
-                        {'Already have an account?'}
-                      </Link>
+                      <Button
+                        className={classes.LinkButton}
+                        style={{ color: "#0645AD" }}
+                        onClick={handleOpen}
+                      >
+                        Already have an account??
+                      </Button>
+                      <Modal
+                        open={open}
+                        onClose={handleClose}
+                        className={classes.modal}
+                      >
+                        <SignIn />
+                      </Modal>
                     </Grid>
                   </Grid>
                 </Box>
@@ -217,7 +233,8 @@ const SignUp = () => {
         </Container>
       </Modal>
     </>
-  )
-}
+  );
+};
 
-export default SignUp
+export { linkClicked };
+export default SignUp;
