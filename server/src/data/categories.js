faf7bdb0a21682716e53461f992d19db6cda82f7
Ashu-tosh-Kr <64960336+Ashu-tosh-Kr@users.noreply.github.com>
2021-10-17 01:27:37 +0530

    updated dummy category coverImage

--- server/src/data/categories.js
+++ server/src/data/categories.js
@@ -5,19 +5,19 @@ const categories = [
   {
     admin: new ObjectID("614ff307cc06fd71f6943615"),
     name: "Electronics",
-    coverImage: "https://source.unsplash.com/PDX_a_82obo/1600x425",
+    coverImage: "https://source.unsplash.com/GMVzbcaTkvc/1600x425",
     description: "Electronics applicances",
   },
   {
     admin: new ObjectID("614ff307cc06fd71f6943615"),
     name: "Stationary",
-    coverImage: "https://source.unsplash.com/pSz3JFTDZMA/1600x425",
+    coverImage: "https://source.unsplash.com/OWPKGmKXbGM/1600x425",
     description: "Pens and pencils",
   },
   {
     admin: new ObjectID("614ff307cc06fd71f6943615"),
     name: "Groceries",
-    coverImage: "https://source.unsplash.com/D6Tu_L3chLE/1600x425",
+    coverImage: "https://source.unsplash.com/i5tesTFPBjw/1600x425",
     description: "Fresh Groceries",
   },
   {
