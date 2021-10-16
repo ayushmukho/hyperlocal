38e07f236cefd187ae1fd359f7b8b80057d4137f
Ashu-tosh-Kr <64960336+Ashu-tosh-Kr@users.noreply.github.com>
2021-10-16 23:00:22 +0530

    fix sendEmail helper func, add category coverImg

--- server/src/api/v1/helpers/sendMail.js
+++ server/src/api/v1/helpers/sendMail.js
@@ -1,62 +1,100 @@
 import nodemailer from "nodemailer";
-import { google } from "googleapis";
+// import { google } from "googleapis";
 import { config } from "dotenv";
 config();
-const { OAuth2 } = google.auth;
-const OAUTH_PLAYGROUND = "https://developers.google.com/oauthplayground";
 
-const {
-  MAILING_SERVICE_CLIENT_ID,
-  MAILING_SERVICE_CLIENT_SECRET,
-  MAILING_SERVICE_REFRESH_TOKEN,
-  SENDER_EMAIL_ADDRESS,
-} = process.env;
+//using google api
+// const { OAuth2 } = google.auth;
+// const OAUTH_PLAYGROUND = "https://developers.google.com/oauthplayground";
 
-const oauth2Client = new OAuth2(
-  MAILING_SERVICE_CLIENT_ID,
-  MAILING_SERVICE_CLIENT_SECRET,
-  MAILING_SERVICE_REFRESH_TOKEN,
-  OAUTH_PLAYGROUND
-);
+// const {
+//   MAILING_SERVICE_CLIENT_ID,
+//   MAILING_SERVICE_CLIENT_SECRET,
+//   MAILING_SERVICE_REFRESH_TOKEN,
+//   SENDER_EMAIL_ADDRESS,
+// } = process.env;
 
-// send mail
-export const sendEmail = (to, url, txt) => {
-  oauth2Client.setCredentials({
-    refresh_token: MAILING_SERVICE_REFRESH_TOKEN,
-  });
+// const oauth2Client = new OAuth2(
+//   MAILING_SERVICE_CLIENT_ID,
+//   MAILING_SERVICE_CLIENT_SECRET,
+//   MAILING_SERVICE_REFRESH_TOKEN,
+//   OAUTH_PLAYGROUND
+// );
 
-  const accessToken = oauth2Client.getAccessToken();
-  const smtpTransport = nodemailer.createTransport({
-    service: "gmail",
+// // send mail
+// export const sendEmail = (to, url, txt) => {
+//   oauth2Client.setCredentials({
+//     refresh_token: MAILING_SERVICE_REFRESH_TOKEN,
+//   });
+
+//   const accessToken = oauth2Client.getAccessToken();
+//   const smtpTransport = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       type: "OAuth2",
+//       user: SENDER_EMAIL_ADDRESS,
+//       clientId: MAILING_SERVICE_CLIENT_ID,
+//       clientSecret: MAILING_SERVICE_CLIENT_SECRET,
+//       refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
+//       accessToken,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: `HyperLocal<${SENDER_EMAIL_ADDRESS}>`,
+//     to: to,
+//     subject: "HyperLocal",
+//     html: `
+//           <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
+//           <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to Hyperlocal</h2>
+//           <p>Congratulations! You're almost set to start using Hyper✮Local.
+//               Just click the button below to validate your email address.
+//           </p>
+
+//           <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</a>
+
+//           </div>
+//       `,
+//   };
+
+//   smtpTransport.sendMail(mailOptions, (err, infor) => {
+//     if (err) return err;
+//     return infor;
+//   });
+// };
+
+//using sendingblue
+export const sendEmail = async (to, url, txt, from = "ashu@hyperlocal.com") => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SENDINGBLUE_HOST,
+    port: process.env.SENDINGBLUE_PORT,
+    secure: false, // true for 465, false for other ports
     auth: {
-      type: "OAuth2",
-      user: SENDER_EMAIL_ADDRESS,
-      clientId: MAILING_SERVICE_CLIENT_ID,
-      clientSecret: MAILING_SERVICE_CLIENT_SECRET,
-      refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
-      accessToken,
+      user: process.env.SENDINGBLUE_EMAIL, // generated ethereal user
+      pass: process.env.SENDINGBLUE_PASS, // generated ethereal password
     },
   });
 
+  // send mail with defined transport object
   const mailOptions = {
-    from: `HyperLocal<${SENDER_EMAIL_ADDRESS}>`,
+    from: `HyperLocal <${from}>`,
     to: to,
     subject: "HyperLocal",
     html: `
-          <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
-          <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to Hyperlocal</h2>
-          <p>Congratulations! You're almost set to start using Hyper✮Local.
-              Just click the button below to validate your email address.
-          </p>
-          
-          <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</a>
-
-          </div>
-      `,
+            <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
+            <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to Hyperlocal</h2>
+            <p>Congratulations! You're almost set to start using Hyper✮Local.
+               Just click the button below to validate your email address.
+            </p>
+            
+             <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</a>
+  
+             </div>
+        `,
   };
-
-  smtpTransport.sendMail(mailOptions, (err, infor) => {
-    if (err) return err;
-    return infor;
+  transporter.sendMail(mailOptions, (error) => {
+    if (error) {
+      return console.log(error);
+    }
   });
 };
--- server/src/data/categories.js
+++ server/src/data/categories.js
@@ -5,25 +5,25 @@ const categories = [
   {
     admin: new ObjectID("614ff307cc06fd71f6943615"),
     name: "Electronics",
-    image: "/images/airpods.jpg",
+    coverImage: "https://source.unsplash.com/PDX_a_82obo/1600x425",
     description: "Electronics applicances",
   },
   {
     admin: new ObjectID("614ff307cc06fd71f6943615"),
     name: "Stationary",
-    image: "/images/airpods.jpg",
+    coverImage: "https://source.unsplash.com/pSz3JFTDZMA/1600x425",
     description: "Pens and pencils",
   },
   {
     admin: new ObjectID("614ff307cc06fd71f6943615"),
     name: "Groceries",
-    image: "/images/airpods.jpg",
+    coverImage: "https://source.unsplash.com/D6Tu_L3chLE/1600x425",
     description: "Fresh Groceries",
   },
   {
     admin: new ObjectID("614ff307cc06fd71f6943615"),
     name: "Services",
-    image: "/images/airpods.jpg",
+    coverImage: "https://source.unsplash.com/NfG4rXmceFM/1600x425",
     description: "Services",
   },
 ];
