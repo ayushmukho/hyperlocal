17fcb42088d6859a6cf2183dcb27877221646e8d
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2021-12-10 21:40:51 +0530

    restructure project

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,20 +1,30 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+baseURL =
+  process.env.NODE_ENV === "production"
+    ? `${process.env.REACT_APP_BASE_URL}/api`
+    : "http://localhost:5000/api";
+const API = axios.create({ baseURL });
 
 /* @ACTIVATION-API */
-export const activation=(id)=>API.post("/auth/activation",{activation_token:id})
-export const userByAccessToken=(access_token)=>API.get("/user/info", { headers: { Authorization: `Bearer ${access_token}` } })
+export const activation = (id) =>
+  API.post("/auth/activation", { activation_token: id });
+export const userByAccessToken = (access_token) =>
+  API.get("/user/info", {
+    headers: { Authorization: `Bearer ${access_token}` },
+  });
 
 /* @SIGNUP-API */
 export const signup = (formData) => API.post("/auth/register", formData);
-export const googleLogin = (token) => API.post("/auth/google_login", {tokenid: token});
+export const googleLogin = (token) =>
+  API.post("/auth/google_login", { tokenid: token });
 
 /* @SIGNIN-API */
 export const signIn = (formData) => API.post("/auth/login", formData);
 
 /* @PRODUCT-CATEGORY-API */
-export const fetchProductsByCategory = (cat) => API.get(`/products/category/${cat}`);
+export const fetchProductsByCategory = (cat) =>
+  API.get(`/products/category/${cat}`);
 
 /* @CART-API */
 export const addToCart = (id) => API.get("/");
@@ -23,8 +33,9 @@ export const addToCart = (id) => API.get("/");
 export const fetchSellers = () => API.get("/seller");
 
 /* @CATEGORIES-API */
-export const fetchCategories = () => API.get('/category')
+export const fetchCategories = () => API.get("/category");
 
 /* @FORGOT PASSWORD-API */
-export const forgotPassword=(email)=>API.post('/auth/forgot',{email})
-export const confirmPassword=(token,password)=>API.post('/auth/reset',{access_token:token,password:password})
+export const forgotPassword = (email) => API.post("/auth/forgot", { email });
+export const confirmPassword = (token, password) =>
+  API.post("/auth/reset", { access_token: token, password: password });
